52aa2a5fa947d927bd088ca6c1d52418be66e0f4
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-08-29 11:28:13 -0700

    Add support for macOS 13

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "nnuninstaller",
     platforms: [
-        .macOS(.v14),
+        .macOS(.v13),
     ],
     products: [
         .executable(
