fc6f7bbec2cd6f0d2c426eae13b459fd1e4ce382
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-09-06 21:16:54 -0700

    Update version to v0.1.2

--- Sources/nnuninstaller/Main/Nnuninstaller.swift
+++ Sources/nnuninstaller/Main/Nnuninstaller.swift
@@ -11,7 +11,7 @@ import ArgumentParser
 struct Nnuninstaller: ParsableCommand {
     static let configuration = CommandConfiguration(
         abstract: "",
-        version: "v0.1.1",
+        version: "v0.1.2",
         subcommands: [
             ListApps.self,
             CheckAppFiles.self,
